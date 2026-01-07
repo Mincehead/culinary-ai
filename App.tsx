07084df8e6aa617666e696c0790b999a5be73d18
Tim D <mincehead@gmail.com>
2026-01-07 11:55:11 +1100

    Fix: Add ScrollToTop component for navigation

--- App.tsx
+++ App.tsx
@@ -6,17 +6,21 @@ import { RecipeDetail } from './components/RecipeDetail';
 import { Profile } from './components/Profile';
 import { PrivacyPolicy } from './components/PrivacyPolicy';
 import { TermsOfService } from './components/TermsOfService';
+import { ScrollToTop } from './components/ScrollToTop';
 
 const App: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/recipes" element={<RecipeList />} />
-      <Route path="/recipe/preview" element={<RecipeDetail />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/privacy" element={<PrivacyPolicy />} />
-      <Route path="/terms" element={<TermsOfService />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/recipes" element={<RecipeList />} />
+        <Route path="/recipe/preview" element={<RecipeDetail />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+        <Route path="/terms" element={<TermsOfService />} />
+      </Routes>
+    </>
   );
 };
 
